c89df55b2505d931c17ad92d0dd1517c78969c2d
mmuenste <64360180+mmuenste@users.noreply.github.com>
2022-09-27 14:06:18 +0200

    Add files via upload

---
+++ netmiko_devices.py
@@ -0,0 +1,9 @@
+devices = [{'ip': '192.168.181.21',
+            'device_type': 'cisco_nxos'},
+           {'ip': '192.168.181.22',
+            'device_type': 'cisco_nxos'},
+           {'ip': '192.168.181.23',
+            'device_type': 'cisco_nxos'},
+           {'ip': '192.168.181.24',
+            'device_type': 'cisco_nxos'}
+           ]
